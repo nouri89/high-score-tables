70199570311d9ef384f423413dcd466a9e0e0dec
Abdennour Hachemi <nouri89@hotmail.com>
2020-10-12 12:29:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./App.css";
 import HighScoreTable from "./HighScoreTable";
 
+
 function App() {
 	return (
 		<div className="App">
-			<h1>High Scores per Country</h1>
+			<h1 style={{textAlign:"center"}}>High Scores per Country</h1>
 
 			<HighScoreTable />
 		</div>
