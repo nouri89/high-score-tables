4d37749f50c092f0a295e6f5e336abaeb52bfc40
Abdennour Hachemi <nouri89@hotmail.com>
2020-10-14 12:20:20 +0100

    Update HighScoreTable.js

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -1,44 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import PlayerScore from "./PlayerScore";
 import scores from "./scores";
 import SortButton from "./SortButton";
-function HighScoreTable(props) {
+function HighScoreTable() {
+	const [currentOrder, setCurrentOrder] = useState("ascending");
+	let arrayOrder = [];
 	const cont = scores;
 
 	const allCountryScores = cont.sort((a, b) => a.name.localeCompare(b.name));
-/*	const currentScoresOrder = allCountryScores.map(function (ele) {
-		return ele.scores;
-	});
-	//console.log(currentScoresOrder);
-	const [scoresOrder1, setScoresOrder] = useState(currentScoresOrder);
-	//console.log(scoresOrder1);
-*/
-	function changeSorting() {
-        console.log("change sorting is called");
-        
+	function changeOrder(chosenOrder) {
+		setCurrentOrder(chosenOrder);
 	}
+
+	function applySorting(array) {
+	
+		if (currentOrder === "ascending") {
+		
+			const newArray = array.sort(function (a, b) {
+				return b.s - a.s;
+			});
+			return newArray;
+		} else {
+			const newArray = array.sort(function (a, b) {
+			return a.s - b.s;
+		});
+		return newArray;
+	
+			
+		}
+
+	}
+	
+
 
 	return (
 		<div>
-            <SortButton onClick={changeSorting} />
+			<SortButton scoreOrder={currentOrder} alterOrder={changeOrder}/>
 			{allCountryScores.map((country) => {
+				arrayOrder = applySorting(country.scores);
+		
 				return (
 					<table>
 						<tr>
-							<th >HIGH SCORES : {country.name} </th>
+							<th>HIGH SCORES : {country.name} </th>
 						</tr>
 
-						{country.scores
-							.sort(function (a, b) {
-								return b.s - a.s;
-							})
-							.map((player) => {
-								return (
-									<td>
-										<PlayerScore pName={player.n} pScore={player.s} />
-									</td>
-								);
-							})}
+						{arrayOrder.map((player) => {
+							return (
+								<td>
+									<PlayerScore pName={player.n} pScore={player.s} />
+								</td>
+							);
+						})}
 					</table>
 				);
 			})}
